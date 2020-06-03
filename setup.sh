f2fdcb4874e012b37c89f4704b05e24d251bd3f8
Nantor <4553079+Nantor@users.noreply.github.com>
2020-06-03 23:23:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,12 @@ sudo apt -y install jq openjdk-8-jre-headless
 
 sudo adduser --system --home $MINECRAFT_HOME $MINECRAFT_USER
 
-sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/getServer.sh \
-&& wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/start.sh \
-&& wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/stop.sh \
-&& chmod +x getServer.sh start.sh stop.sh \
-&& (crontab -l 2>/dev/null; echo \"4 44 * * * /bin/bash $MINECRAFT_HOME/stop.sh && /bin/bash $MINECRAFT_HOME/start.sh\") | crontab - \
-&& (crontab -l 2>/dev/null; echo \"@reboot /bin/bash $MINECRAFT_HOME/start.sh\") | crontab - \
-&& echo eula=true > eula.txt \
-&& ./getServer.sh \
-&& nohup ./start.sh" > /dev/null
+sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/getServer.sh"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/start.sh"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/stop.sh"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "chmod +x getServer.sh start.sh stop.sh"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"4 44 * * * /bin/bash $MINECRAFT_HOME/stop.sh && /bin/bash $MINECRAFT_HOME/start.sh\") | crontab -"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"@reboot /bin/bash $MINECRAFT_HOME/start.sh\") | crontab -"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "echo eula=true > eula.txt"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "./getServer.sh"
+sudo su - $MINECRAFT_USER -s /bin/bash -c "nohup ./start.sh" > /dev/null
