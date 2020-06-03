61bb11518952f468cfa1c9c965dfc853cf7d3bde
Nantor <4553079+Nantor@users.noreply.github.com>
2020-06-03 21:52:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+$MINECRAFT_USER=minecraft
+$MINECRAFT_HOME=/home/$MINECRAFT_USER
+
 sudo apt update
 sudo apt -y install jq openjdk-8-jre-headless
 
+sudo adduser --system --home $MINECRAFT_HOME $MINECRAFT_USER
+
+sudo su - minecraft -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/getServer.sh \
+&& wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/start.sh \
+&& wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/stop.sh \
+&& chmod +x getServer.sh start.sh stop.sh \
+&& (crontab -l 2>/dev/null; echo \"4 44 * * * /bin/bash $MINECRAFT_HOME\stop.sh && /bin/bash $MINECRAFT_HOME\start.sh\") | crontab - \
+&& (crontab -l 2>/dev/null; echo \"@reboot /bin/bash $MINECRAFT_HOME\start.sh\") | crontab - \
+&& echo eula=true > eula.txt \
+&& ./getServer.sh \
+&& nohup ./start.sh" > /dev/null
