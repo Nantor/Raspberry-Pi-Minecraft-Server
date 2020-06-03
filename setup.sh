9178adc1ca00881ad41a757a8161464afa0dfe62
Nantor <4553079+Nantor@users.noreply.github.com>
2020-06-04 00:40:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-MINECRAFT_USER="minecraft"
-MINECRAFT_HOME="/home/$MINECRAFT_USER"
-
 sudo apt update
 sudo apt -y install jq openjdk-8-jre-headless
 
-sudo adduser --system --home $MINECRAFT_HOME $MINECRAFT_USER
+sudo adduser --system --home /home/minecraft minecraft
 
-sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/getServer.sh"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/start.sh"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/stop.sh"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "chmod +x getServer.sh start.sh stop.sh"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"4 44 * * * /bin/bash $MINECRAFT_HOME/stop.sh && /bin/bash $MINECRAFT_HOME/start.sh\") | crontab -"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"@reboot /bin/bash $MINECRAFT_HOME/start.sh\") | crontab -"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "echo eula=true > eula.txt"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "./getServer.sh"
-sudo su - $MINECRAFT_USER -s /bin/bash -c "nohup ./start.sh" > /dev/null
+sudo su - minecraft -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/getServer.sh" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/start.sh" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "wget -q https://github.com/Nantor/Raspberry-Pi-Minecraft-Server/raw/master/stop.sh" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "chmod +x getServer.sh start.sh stop.sh" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"4 44 * * * /bin/bash /home/minecraft/stop.sh && /bin/bash /home/minecraft/start.sh\") | crontab -" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "(crontab -l 2>/dev/null; echo \"@reboot /bin/bash /home/minecraft/start.sh\") | crontab -" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "echo eula=true > eula.txt" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "./getServer.sh" 2>&1 > /dev/null
+sudo su - minecraft -s /bin/bash -c "nohup ./start.sh" 2>&1 > /dev/null
